4cb8de390ea27d5d177614aedcd8770bd9385571
abutolib <abutolib12042002@gmail.com>
2022-11-24 18:29:41 +0500

    Innitial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// var userName = prompt('Ismingizni kiriting :','Abutolib');
+// var userLastName = prompt('Familyangizni kiriting :',"Fayzullayev");
+// var userAge = prompt('Yoshingizni kiriting :',20);
+// var userStudent = confirm('Studentmisiz :',true);
+// var userRegion = prompt('Viloyatni kiriting :',"Jizzax");
+// var userPhoneNumber = prompt('Raqamingizni kiriting :',"+998943453466");
+// console.log("user_first_name : " + " " + userName + " " + ";");
+// console.log("user_last_name : " + " " + userLastName + " " + ";");
+// console.log("user_age : " + " " + userAge + " " + ";");
+// console.log("user_is_student : " + " " + userStudent + " " + ";");
+// console.log("user_region : " + " " + userRegion + " " + ";");
+// console.log("user_phone_number : " + " " + userPhoneNumber + " " + ";");
+
+
+
+var userName = prompt('Ismingizni kiriting :');
+
+var userReady = confirm('Tayyormisiz:');
+
+var test1 = prompt("CSS da komentariya qanday ko'rinishida bo'ladi? \n A)/* --------  */ \n B) '--------' \n C)// ------ // \n D) // -------");
+
+var test2 = prompt("Bootstrap qaysi kompaniya tomonidan yaratilgan?\n A)Facebook \n B) Twitter \n C)Instagram \n D) Telegram");
+
+var test3 = prompt("Fontlar oilasi asosan nechta?\n A)5 \n B) 4 \n C)3 \n D) 2");
+var test4 = confirm("Inputda \"week\" degan type mavjud emasmi");
+var test5 = prompt("Font-weightda \"Lite\" nomli qalinlikqaysi biriga to'g'ri keladi?\n A)600 \n B) 300 \n C)400 \n D)100");
+console.log("Test 1" + '\n' + 'User javobi :' + test1 + ' ' + "To'g'ri javob:A")
+console.log("Test 2" + '\n' + 'User javobi :' + test2 + ' ' + "To'g'ri javob:B")
+console.log("Test 3" + '\n' + 'User javobi :' + test3 + ' ' + "To'g'ri javob:B")
+console.log("Test 4" + '\n' + 'User javobi :' + test4 + ' ' + "To'g'ri javob:False")
+console.log("Test 5" + '\n' + 'User javobi :' + test5 + ' ' + "To'g'ri javob:B")
